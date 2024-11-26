481ecad0f5d1ac0f92a1bb902ef5afba598322b6
Rosa Moriya <155775675+rosamoriya@users.noreply.github.com>
2024-11-26 15:47:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Placeholder for future interactive functionality
+console.log("Script loaded!");
+
+// Example: Adding functionality to "Learn More" button
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Learn more about this dinosaur!");
+  });
+});
